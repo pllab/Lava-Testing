ffc7ae4dbe5d00d00ef03e9223d0f6d9944ce54e
Michael Christensen <chmdko@gmail.com>
2021-01-11 17:53:28 -0800

    playing with row

--- chalmers-lava/chalmers-lava-testing/Main.hs
+++ chalmers-lava/chalmers-lava-testing/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Lava
 import qualified Lava.Arithmetic as LA
+import qualified Lava.Patterns as LP
 
 (|>) x f = f x
 
@@ -21,11 +22,15 @@ fullAdd (cin, (a, b)) = (sum, cout)
     (sum, carry2)  = halfAdd (cin, sum1)
     cout           = Lava.xor2 (carry2, carry1)
 
+adder :: (Lava.Signal Bool, ([Lava.Signal Bool], [Lava.Signal Bool])) -> ([Lava.Signal Bool], Lava.Signal Bool)
 adder (cin, ([], [])) = ([], cin)
 adder (cin, (a:as, b:bs)) = (sum:sums, cout)
   where
     (sum, carry) = fullAdd (cin, (a, b))
     (sums, cout) = adder (carry, (as, bs))
+
+adder' :: (Lava.Signal Bool, [(Lava.Signal Bool, Lava.Signal Bool)]) -> ([Lava.Signal Bool], Lava.Signal Bool)
+adder' = LP.row fullAdd
   
 add n (a, b) = out
   where
@@ -38,15 +43,24 @@ main :: IO ()
 main = do
   test1
   test2
+  test3
 
 -- Combinational examples
+-- 1) Half adder
 test1 = do
   Lava.simulateSeq halfAdd Lava.domain |> mapM_ print
   Lava.writeVhdl "halfAdd" halfAdd
   Lava.satzoo prop_HalfAddOutputNeverBothTrue >>= print
 
+-- 2) Full adder
 test2 = do
   Lava.simulate (add 16) (4, 5) |> print
   Lava.writeVhdlInputOutput "4BitAdder" adder
     (Lava.var "cin", (Lava.varList 4 "a", Lava.varList 4 "b"))
+    (Lava.varList 4 "sum", Lava.var "cout")
+
+-- 3) Full adder (via row)
+test3 = do
+  Lava.writeVhdlInputOutput "4BitAdder_row" adder'
+    (Lava.var "cin", map (\i -> (Lava.var $ "a_" ++ show i, Lava.var $ "b_" ++ show i)) [0..4])
     (Lava.varList 4 "sum", Lava.var "cout")
---
+++ chalmers-lava/chalmers-lava-testing/README.md
@@ -0,0 +1,5 @@
+Hackage information:
+* https://hackage.haskell.org/package/chalmers-lava2000
+
+Great tutorial doc:
+* http://projects.haskell.org/chalmers-lava2000/Doc/tutorial.pdf
